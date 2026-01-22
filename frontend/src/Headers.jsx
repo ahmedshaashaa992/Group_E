bc9f0f18e3766d1ede6ebe854fb13a03ce37d009
nawar993 <nawar.fr93@gmail.com>
2026-01-22 20:04:36 +0100

    add nawar

--- frontend/src/Headers.jsx
+++ frontend/src/Headers.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 const Headers = () => {
   return (
     <>
-    <div><h1>Welcome to Group E react platform</h1></div>
-        <div><h1 style={{color:"red"}}>Teacher: Ahmed Shaashaa</h1></div>
-        <div><h1 style={{color:"yellow"}}>Students:</h1></div>
+      <div><h1>Welcome to Group E react platform</h1></div>
+      <div><h1 style={{color:"red"}}>Teacher: Ahmed Shaashaa</h1></div>
+      <div><h1 style={{color:"yellow"}}>Students:</h1></div>
+      <div><h2 style={{color:"blue"}}>Nawar</h2></div>
     </>
   )
 }
