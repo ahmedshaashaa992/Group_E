1e7c4a15acdd1f40c9f280496da6d52e137521db
ahmedshaashaa992 <ahmed.sh.adtco@icloud.com>
2026-01-22 18:56:32 +0100

    update app.jsx and add headers also prepare the build for production

---
+++ frontend/src/Headers.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Headers = () => {
+  return (
+    <>
+    <div><h1>Welcome to Group E react platform</h1></div>
+        <div><h1 style={{color:"red"}}>Teacher: Ahmed Shaashaa</h1></div>
+    </>
+  )
+}
+
+export default Headers
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  //base: "/Group_E/",
+  base: process.env.BASE_URL || "/",
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+  preview: {
+    port: 8080,
+    host: true
+  }
 })
