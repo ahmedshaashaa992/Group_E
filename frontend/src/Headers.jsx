4c2837a05f79495ac64c02f0452d3bbd2d132420
ahmedshaashaa992 <ahmed.sh.adtco@icloud.com>
2026-01-22 19:09:05 +0100

    update headers adding students header

--- frontend/src/Headers.jsx
+++ frontend/src/Headers.jsx
@@ -5,6 +5,7 @@ const Headers = () => {
     <>
     <div><h1>Welcome to Group E react platform</h1></div>
         <div><h1 style={{color:"red"}}>Teacher: Ahmed Shaashaa</h1></div>
+        <div><h1 style={{color:"blue"}}>Students:</h1></div>
     </>
   )
 }
